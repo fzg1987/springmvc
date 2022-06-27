d6ae08e809b7b09e3e6c78628260bed511f87615
fzg1987 <beginer@yeah.net>
2022-06-27 20:28:16 +0800

    Spring MVC参数绑定

---
+++ src/main/java/com/fzg/controller/RESTHandler.java
@@ -0,0 +1,30 @@
+package com.fzg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/rest")
+public class RESTHandler {
+
+//    @RequestMapping(value = "/find",method = RequestMethod.GET)
+    @GetMapping("/find")
+    @ResponseBody
+    public String find(){
+        return "Hello";
+    }
+    @PostMapping("/save")
+    public void save(){
+    }
+    @PutMapping("/update")
+    @ResponseBody
+    public String update(HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        return "已接收到PUT请求";
+    }
+    @DeleteMapping("/delete")
+    public void delete(){
+    }
+}
---
+++ src/main/webapp/test.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: fzg1987
+  Date: 2022/6/27
+  Time: 18:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/rest/update" method="post">
+    <input type="hidden" name="_method" value="PUT" />
+    <input type="submit" value="提交" />
+</form>
+</body>
+</html>
